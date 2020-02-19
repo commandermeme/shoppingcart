8f4baed67323311409ca99072c314a4e0a77b038
Cossette Alexis Gabuya <gabuyalexis@gmail.com>
2020-02-20 03:42:11 +0800

    Update shopping_cart.py

--- shopping_cart.py
+++ shopping_cart.py
@@ -20,29 +20,69 @@ window.geometry("{}x{}".format(screen_width, screen_height))
 welcome_label = Label(window, font='Calibri 40', bg='black', fg='gray', text='Welcome to Abcd Efgh!')
 welcome_label.place(relx=0.5, rely=0.5, anchor=CENTER)
 
+left_frame = Frame(window,width=400, height=742, bg='gray')
+right_frame = Frame(window, width=950, height=742, bg='pink')
+
+
+
 def shop_now():
     global male_button
     global female_button
     welcome_label.place_forget()
     shopnow_button.place_forget()
-    male_button = Button(window, text="Male", command=male_products)
+    male_button = Button(window, text="Male", command=products_menu)
     male_button.place(relx=0.3, rely=0.5, anchor=CENTER)
-    female_button = Button(window, text="Female", command=female_products)
+    female_button = Button(window, text="Female", command=products_menu)
     female_button.place(relx=0.6, rely=0.5, anchor=CENTER)
 
-def male_products():
-    limit = 1
-    x = 0.15
-    y = 0.5
-    global left_frame
-    global right_frame
+# def male_products():
+#     limit = 1
+#     x = 0.15
+#     y = 0.5
+#     male_button.place_forget()
+#     female_button.place_forget()
+#     left_frame.place(relx=0.15, rely=0.5, anchor=CENTER)
+#     right_frame.place(relx=0.645, rely=0.5, anchor=CENTER)
+
+#     cursorObj.execute('SELECT * FROM male_products')
+#     rows = cursorObj.fetchall()
+#     for row in rows:
+#         print(row)
+#         item = Canvas(right_frame, width=100, height=100)
+#         item.pack(side=LEFT, padx=5)
+#         right_frame.pack_propagate(False)
+#         right_frame.update()
+
+# def female_products():
+#     male_button.place_forget()
+#     female_button.place_forget()
+#     left_frame = Frame(window)
+#     left_frame.place()
+#     right_frame = Frame(window, bg='gray')
+#     right_frame.place(relx=0.6, rely=0.5, anchor=E)
+#     window.configure(bg='pink')
+
+def products_menu():
     male_button.place_forget()
     female_button.place_forget()
-    left_frame = Frame(window,width=400, height=742, bg='gray')
     left_frame.place(relx=0.15, rely=0.5, anchor=CENTER)
-    right_frame = Frame(window, width=950, height=742, bg='pink')
     right_frame.place(relx=0.645, rely=0.5, anchor=CENTER)
+    products_label = Label(left_frame, text='Products')
+    products_label.place(relx=0.6, rely=0.4, anchor=E)
+    variable = StringVar(left_frame)
+    variable.set("Products")
+    products_menu = OptionMenu(left_frame, variable, 'Eyeglasses', 'Necklace', 'Earings', command=products_category)
+    products_menu.place(relx=0.6, rely=0.5, anchor=E)
 
+def products_category(choice): 
+    if choice == 'Eyeglasses':
+        male_eyeglasses()
+    elif choice == 'Necklace': 
+        print('Necklace')
+    else:
+        print('Earrings')
+
+def male_eyeglasses():
     cursorObj.execute('SELECT * FROM male_products')
     rows = cursorObj.fetchall()
     for row in rows:
@@ -51,15 +91,6 @@ def male_products():
         item.pack(side=LEFT, padx=5)
         right_frame.pack_propagate(False)
         right_frame.update()
-
-def female_products():
-    male_button.place_forget()
-    female_button.place_forget()
-    left_frame = Frame(window)
-    left_frame.place()
-    right_frame = Frame(window, bg='gray')
-    right_frame.place(relx=0.6, rely=0.5, anchor=E)
-    window.configure(bg='pink')
 
 shopnow_button = Button(window, text="Shop now!", command=shop_now)
 shopnow_button.place(relx=0.5,rely=0.6, anchor=CENTER)
