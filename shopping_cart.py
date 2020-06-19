6d36333cfc4dcffc694c41fd68c7dcfb2262aac5
Jaymar Lagramada <50193001+commandermeme@users.noreply.github.com>
2020-06-19 16:10:14 +0800

    Update shopping_cart.py

--- shopping_cart.py
+++ shopping_cart.py
@@ -17,7 +17,7 @@ screen_width = window.winfo_screenwidth()
 screen_height = window.winfo_screenheight()
 window.geometry("{}x{}".format(screen_width, screen_height))
 
-welcome_label = Label(window, font='Calibri 40', bg='black', fg='gray', text='Welcome to Abcd Efgh!')
+welcome_label = Label(window, font='Calibri 40', bg='black', fg='gray', text='Welcowwwme to Abcd Efgh!')
 welcome_label.place(relx=0.5, rely=0.5, anchor=CENTER)
 
 left_frame = Frame(window,width=400, height=742, bg='gray')
